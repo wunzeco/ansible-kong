7d8d1708deabff0b0962427f8c29b0acdcab536c
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-03-31 09:53:10 +0100

    [OI] Check kong running status. Add tests for kong processes

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 kong_conf_dir = '/etc/kong/'
+kong_nginx_working_dir = '/usr/local/kong'
 
 
 describe package('kong') do
   it { should be_installed }
 end
 
-describe file("#{kong_conf_dir}") do
-  it { should be_directory }
-  it { should be_mode 755 }
-  it { should be_owned_by 'root' }
+%W(
+  #{kong_conf_dir}
+  #{kong_nginx_working_dir}
+).each do |d|
+  describe file(d) do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'root' }
+  end
 end
 
 describe file("#{kong_conf_dir}/kong.yml") do
@@ -27,4 +33,18 @@ end
 
 describe command("/usr/local/bin/kong version") do
   its(:stdout) { should match %r(Kong version: 0.*) }
+end
+
+describe process("serf") do
+  it { should be_running }
+  its(:args) { should match %r(agent -profile=wan -rpc-addr=.*:kong=/usr/local/kong/serf_event.sh) }
+end
+
+describe process("nginx") do
+  it { should be_running }
+  its(:args) { should match %r(-p /usr/local/kong -c nginx.conf) }
+end
+
+describe process("dnsmasq") do
+  it { should be_running }
 end
